bd1b7efbbe531dca55308515f5fe5f532999b7e1
Nick Ryan <nickryan@Nicks-MBP-2.lan>
2023-03-15 20:49:33 -0500

    initialized git repo

---
+++ elementor-addon.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Elementor Custom Widgets
+ * Description: Adds additional widgets to Elementor.
+ * Plugin URI:  https://elementor.com/
+ * Version:     1.0.0
+ * Author:      hrefnick
+ * Author URI:  https://hrefnick.com/
+ * Text Domain: elementor-addon
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * Register oEmbed Widget.
+ *
+ * Include widget file and register widget class.
+ *
+ * @since 1.0.0
+ * @param \Elementor\Widgets_Manager $widgets_manager Elementor widgets manager.
+ * @return void
+ */
+function register_hrefnick_gallery_widget( $widgets_manager ) {
+
+    require_once( __DIR__ . '/widgets/hrefnick-gallery-widget.php' );
+
+    $widgets_manager->register( new \Elementor_Hrefnick_Gallery_Widget() );
+
+}
+add_action( 'elementor/widgets/register', 'register_hrefnick_gallery_widget' );
